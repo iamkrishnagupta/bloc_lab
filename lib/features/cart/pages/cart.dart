d4d13649336cd962c0163621798368904d8bd72f
Krishna Gupta <krishna.ghhh@gmail.com>
2024-08-17 13:03:19 +0530

    changed cart page UI

--- lib/features/cart/pages/cart.dart
+++ lib/features/cart/pages/cart.dart
@@ -1,4 +1,10 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../home/pages/product_tile_widget.dart';
+import '../bloc/cart_bloc.dart';
 import 'package:flutter/material.dart';
+
+import 'cart_tile_widget.dart';
 
 class Cart extends StatefulWidget {
   const Cart({super.key});
@@ -8,8 +14,40 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
+  final CartBloc cartBloc = CartBloc();
+
+  @override
+  void initState() {
+    cartBloc.add(CartInitialEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Cart'),
+        ),
+        body: BlocConsumer<CartBloc, CartState>(
+          bloc: cartBloc,
+          listenWhen: (previous, current) => current is CartActionState,
+          buildWhen: (previous, current) => current is! CartActionState,
+          listener: (context, state) {},
+          builder: (context, state) {
+            switch (state.runtimeType) {
+              case const (CartSuccessState):
+                return ListView.builder(
+                  itemCount: (state as CartSuccessState).cartItems.length,
+                  itemBuilder: (context, index) {
+                    return CartTile(
+                      productDataModel: state.cartItems[index],
+                      cartBloc: cartBloc,
+                    );
+                  },
+                );
+            }
+            return const SizedBox();
+          },
+        ));
   }
 }
