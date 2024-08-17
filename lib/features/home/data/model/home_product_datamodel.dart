7e5cf2f8202a3b4fabb9f6778d997a945b815ff3
Krishna Gupta <krishna.ghhh@gmail.com>
2024-08-17 09:20:23 +0530

    added product data model

---
+++ lib/features/home/data/model/home_product_datamodel.dart
@@ -0,0 +1,15 @@
+class ProductDataModel {
+  final int id;
+  final String description;
+  final String imageUrl;
+  final double price;
+  final String name;
+
+  ProductDataModel({
+    required this.id,
+    required this.description,
+    required this.imageUrl,
+    required this.price,
+    required this.name,
+  });
+}
