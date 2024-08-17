9ec0d3c37b0f9ed1fa300e690d9f9b762cadbe1e
Krishna Gupta <krishna.ghhh@gmail.com>
2024-08-17 13:03:40 +0530

    add cart states

--- lib/features/cart/bloc/cart_state.dart
+++ lib/features/cart/bloc/cart_state.dart
@@ -2,5 +2,14 @@ part of 'cart_bloc.dart';
 
 @immutable
 sealed class CartState {}
+sealed class CartActionState extends CartState {}
+
 
 final class CartInitial extends CartState {}
+
+class CartSuccessState extends CartState {
+  final List<ProductDataModel> cartItems;
+
+  CartSuccessState({required this.cartItems});
+}
+
