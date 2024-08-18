ba9f253b4a1c88a796478d5818e44dbd5f556c92
Krishna Gupta <krishna.ghhh@gmail.com>
2024-08-18 09:34:14 +0530

    added on wishlist event

--- lib/features/wishlist/bloc/wishlist_bloc.dart
+++ lib/features/wishlist/bloc/wishlist_bloc.dart
@@ -1,13 +1,22 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+
+
+import '../../../data/wishlist_items.dart';
+import '../../home/data/model/home_product_datamodel.dart';
 
 part 'wishlist_event.dart';
 part 'wishlist_state.dart';
 
 class WishlistBloc extends Bloc<WishlistEvent, WishlistState> {
   WishlistBloc() : super(WishlistInitial()) {
-    on<WishlistEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<WishlistInitialEvent>(wishlistInitialEvent);
   }
+
+  FutureOr<void> wishlistInitialEvent(
+      WishlistInitialEvent event, Emitter<WishlistState> emit) {
+    emit(WishlistSuccessState(wishlistItems: wishlistItems));
+      }
 }
