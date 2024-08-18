0b20ca927dedc44b21de7a2faaecc7af9b3dfedb
Krishna Gupta <krishna.ghhh@gmail.com>
2024-08-18 12:42:57 +0530

    added on removebuttonclickevent func

--- lib/features/cart/bloc/cart_bloc.dart
+++ lib/features/cart/bloc/cart_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 import '../../../data/cart_items.dart';
@@ -12,10 +13,24 @@ part 'cart_state.dart';
 class CartBloc extends Bloc<CartEvent, CartState> {
   CartBloc() : super(CartInitial()) {
     on<CartInitialEvent>(cartInitialEvent);
+    on<CartRemoveButtonClickedEvent>(cartRemoveButtonClickedEvent);
+
   }
 
   FutureOr<void> cartInitialEvent(
       CartInitialEvent event, Emitter<CartState> emit) {
     emit(CartSuccessState(cartItems: cartItems));
+  }
+
+   FutureOr<void> cartRemoveButtonClickedEvent(
+      CartRemoveButtonClickedEvent event, Emitter<CartState> emit) {
+    debugPrint('Remove item from Cart Clicked.');
+    cartItems.remove(event.product);
+    emit(ProductRemovedFromCartActionState());
+
+    //note: emit a new state with the updated cartlist
+    emit(CartSuccessState(cartItems: cartItems));
+
+
   }
 }
