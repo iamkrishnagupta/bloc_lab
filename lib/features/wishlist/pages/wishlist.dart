0a6179a5cc78d7f3fbec1bd20ba64cddc4349a0b
Krishna Gupta <krishna.ghhh@gmail.com>
2024-08-18 09:36:50 +0530

    modified wishlist.dart

--- lib/features/wishlist/pages/wishlist.dart
+++ lib/features/wishlist/pages/wishlist.dart
@@ -1,3 +1,7 @@
+import 'package:bloc_lab/features/wishlist/pages/wishlist_tile_widget.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../data/wishlist_items.dart';
+import '/features/wishlist/bloc/wishlist_bloc.dart';
 import 'package:flutter/material.dart';
 
 class Wishlist extends StatefulWidget {
@@ -8,8 +12,40 @@ class Wishlist extends StatefulWidget {
 }
 
 class _WishlistState extends State<Wishlist> {
+  final WishlistBloc wishlistBloc = WishlistBloc();
+
+  @override
+  void initState() {
+    wishlistBloc.add(WishlistInitialEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Wishlist'),
+        ),
+        body: BlocConsumer<WishlistBloc, WishlistState>(
+          bloc: wishlistBloc,
+          listenWhen: (previous, current) => current is WishlistActionState,
+          buildWhen: (previous, current) => current is! WishlistActionState,
+          listener: (context, state) {},
+          builder: (context, state) {
+            switch (state.runtimeType) {
+              case const (WishlistSuccessState):
+                return ListView.builder(
+                  itemCount: (state as WishlistSuccessState).wishlistItems.length,
+                  itemBuilder: (context, index) {
+                    return WishlistTile(
+                      productDataModel: state.wishlistItems[index],
+                      wishlistBloc: wishlistBloc,
+                    );
+                  },
+                );
+            }  
+            return const SizedBox();
+          },
+        ));
   }
 }
