23ccca5550ab87cfa17944f80519f0cf751ae0b4
Krishna Gupta <krishna.ghhh@gmail.com>
2024-08-18 11:22:09 +0530

    modified bloc for wishlist

--- lib/features/wishlist/bloc/wishlist_bloc.dart
+++ lib/features/wishlist/bloc/wishlist_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 import '../../../data/wishlist_items.dart';
@@ -12,10 +13,23 @@ part 'wishlist_state.dart';
 class WishlistBloc extends Bloc<WishlistEvent, WishlistState> {
   WishlistBloc() : super(WishlistInitial()) {
     on<WishlistInitialEvent>(wishlistInitialEvent);
+    on<WishlistRemoveButtonClickedEvent>(wishlistRemoveButtonClickedEvent);
   }
 
   FutureOr<void> wishlistInitialEvent(
       WishlistInitialEvent event, Emitter<WishlistState> emit) {
     emit(WishlistSuccessState(wishlistItems: wishlistItems));
+  }
+
+  FutureOr<void> wishlistRemoveButtonClickedEvent(
+      WishlistRemoveButtonClickedEvent event, Emitter<WishlistState> emit) {
+    debugPrint('Remove item from Wishlist Clicked.');
+    wishlistItems.remove(event.product);
+    emit(ProductRemovedFromWishlistActionState());
+
+    //note: emit a new state with the updated wishlist
+    emit(WishlistSuccessState(wishlistItems: wishlistItems));
+
+
   }
 }
