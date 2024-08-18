cda74e130a5a1cf93cd314462bee2965ec60d9ca
Krishna Gupta <krishna.ghhh@gmail.com>
2024-08-18 09:37:41 +0530

    added wishlist states
    action and initial states

--- lib/features/wishlist/bloc/wishlist_state.dart
+++ lib/features/wishlist/bloc/wishlist_state.dart
@@ -2,5 +2,13 @@ part of 'wishlist_bloc.dart';
 
 @immutable
 sealed class WishlistState {}
+sealed class WishlistActionState extends WishlistState {}
 
 final class WishlistInitial extends WishlistState {}
+
+
+class WishlistSuccessState extends WishlistState {
+  final List<ProductDataModel> wishlistItems;
+
+  WishlistSuccessState({required this.wishlistItems});
+}
